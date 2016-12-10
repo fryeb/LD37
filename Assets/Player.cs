94dbd8d439bb64b588a090cac5e94f7f51943504
Ben Frye <ben.j.frye@gmail.com>
2016-12-10 16:37:33 +1100

    Added UI Raycasting

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,13 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 public class Player : MonoBehaviour
 {
 	public Vector2 mouseSensitivity = Vector2.one;
 
 	void Update()
 	{
-		transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivity.x, 0);
-		Camera.main.transform.Rotate(Input.GetAxis("Mouse Y") * -mouseSensitivity.y, 0, 0);
+		Vector2 look = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
+		transform.Rotate(0, look.x * mouseSensitivity.x, 0);
+		Camera.main.transform.Rotate(look.y * -mouseSensitivity.y, 0, 0);
+
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+		PointerEventData pointerData = new PointerEventData(EventSystem.current);
+
+		pointerData.position = Input.mousePosition;
+
+		List<RaycastResult> results = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(pointerData, results);
+
+		if (results.Count > 0)
+		{
+			if (results[0].gameObject.layer == LayerMask.NameToLayer("UI"))
+			{
+				string dbg = "Root Element: {0} \n GrandChild Element: {1}";
+				Debug.Log(string.Format(dbg, results[results.Count - 1].gameObject.name, results[0].gameObject.name));
+				results.Clear();
+			}
+		}
 	}
 }
