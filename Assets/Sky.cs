9800ed1c79f5b86f60786d4d772da1922402a83d
Ben Frye <ben.j.frye@gmail.com>
2016-12-10 22:51:28 +1100

    Started Sky Puzzle

---
+++ Assets/Sky.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sky : MonoBehaviour {
+
+	public string answer;
+
+	private Button reset;
+	private Textbox equation_1;
+
+	void Start () 
+	{
+		reset = transform.FindChild("reset").GetComponent<Button>();
+		equation_1 = transform.FindChild("equation_1").GetComponent<Textbox>();
+	}
+
+	void Update () 
+	{
+		if (equation_1.entered == answer)
+		{
+
+		}
+	}
+}
