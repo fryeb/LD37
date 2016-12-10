126f4f249f0c74db4bdfa248fb9a519cbc6e4097
Ben Frye <ben.j.frye@gmail.com>
2016-12-10 20:28:21 +1100

    Added Off

---
+++ Assets/Off.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Off : MonoBehaviour
+{
+
+	private Button power;
+
+	void Start()
+	{
+		power = transform.FindChild("power").GetComponent<Button>();
+	}
+
+	void Update()
+	{
+		if (power.clicked)
+			PuzzleController.LoadPuzzle("Login");
+	}
+}
