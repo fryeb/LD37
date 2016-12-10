ac2909f0a104065052f87bffeccae4089235f2d8
Ben Frye <ben.j.frye@gmail.com>
2016-12-11 04:54:59 +1100

    Started working on main menu

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public float TurnSpeed = 1.0f;
+
+	private Transform SkyDome;
+
+	void Start()
+	{
+		SkyDome = GameObject.Find("SkyDome").transform;
+	}
+
+	void Update()
+	{
+		SkyDome.Rotate(0f, TurnSpeed * Time.deltaTime, 0f);
+	}
+
+	void PlayNormal()
+	{
+		
+	}
+
+	void PlayZombies()
+	{
+		
+	}
+
+	void Quit()
+	{
+		Application.Quit();
+	}
+}
