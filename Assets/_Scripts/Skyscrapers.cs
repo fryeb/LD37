54d035a708737d68fb2b2b97dc02ed76ec41d037
Ben Frye <ben.j.frye@gmail.com>
2016-12-11 03:45:28 +1100

    Added Life Puzzle

--- Assets/_Scripts/Skyscrapers.cs
+++ Assets/_Scripts/Skyscrapers.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Skyscrapers : MonoBehaviour {
 
+	public float stopMinimum;
+	public float stopMaximum;
+
 	private Button start;
 	private Button stop;
 	private Button restore;
@@ -28,6 +31,39 @@ public class Skyscrapers : MonoBehaviour {
 
 	void Update () 
 	{
-	
+		if (stop.clicked)
+		{
+			mover_1.moving = false;
+			mover_2.moving = false;
+			mover_3.moving = false;
+
+			bool mover_1_inrange = mover_1.fill < stopMaximum && mover_1.fill > stopMinimum;
+			bool mover_2_inrange = mover_2.fill < stopMaximum && mover_2.fill > stopMinimum;
+			bool mover_3_inrange = mover_3.fill < stopMaximum && mover_3.fill > stopMinimum;
+
+			if (mover_1_inrange && mover_2_inrange && mover_3_inrange)
+				restore.gameObject.SetActive(true);
+			else
+				start.gameObject.SetActive(true);
+
+			stop.gameObject.SetActive(false);
+		}
+
+		if (start.clicked)
+		{
+			start.gameObject.SetActive(false);
+			stop.gameObject.SetActive(true);
+
+			mover_1.moving = true;
+			mover_2.moving = true;
+			mover_3.moving = true;
+		}
+
+		if (restore.clicked)
+		{
+			WorldController.UnlockElement("Skyscrapers");
+			Desktop.skyscrapers_complete = true;
+			PuzzleController.LoadPuzzle("Desktop");
+		}
 	}
 }
