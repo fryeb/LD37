6442cbce3002a05675b4332843d19e23e70a7010
Ben Frye <ben.j.frye@gmail.com>
2016-12-10 16:13:54 +1100

    Added First Person Camera

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public Vector2 mouseSensitivity = Vector2.one;
+
+	void Update()
+	{
+		transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivity.x, 0);
+		Camera.main.transform.Rotate(Input.GetAxis("Mouse Y") * -mouseSensitivity.y, 0, 0);
+	}
+}
