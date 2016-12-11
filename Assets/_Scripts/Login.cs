fbac879245cc83097e0056b98e01ee49267cec85
Ben Frye <ben.j.frye@gmail.com>
2016-12-11 15:40:49 +1100

    Cleaned Up Login Screen

--- Assets/_Scripts/Login.cs
+++ Assets/_Scripts/Login.cs
@@ -4,17 +4,24 @@ using System.Collections;
 public class Login : MonoBehaviour {
 
 	public string password_text = "1234";
+
 	private Textbox password;
+	private Button start;
 
 	void Start () 
 	{
 		password = transform.FindChild("password").GetComponent<Textbox>();
+		start = transform.FindChild("start").GetComponent<Button>();
+		start.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		if (password.entered == password_text)
+			start.gameObject.SetActive(true);
+
+		if (start.clicked)
 			PuzzleController.LoadPuzzle("Safemode");
 	}
 }
