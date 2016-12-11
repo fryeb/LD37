52e15a839d5424d49ac73295067cfc9b28025473
Ben Frye <ben.j.frye@gmail.com>
2016-12-11 11:48:02 +1100

    Added SuperError Screen

---
+++ Assets/_Scripts/SuperError.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperError : MonoBehaviour {
+
+	public float delay = 1.0f;
+
+	private float elapsed = 1.0f;
+	private int currentChild = 0;
+
+	void Start()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			transform.GetChild(i).gameObject.SetActive(false);
+		};
+	}
+
+	void Update () 
+	{
+		if (currentChild < transform.childCount)
+		{
+			elapsed += Time.deltaTime;
+
+			if (elapsed >= delay)
+			{
+				transform.GetChild(currentChild).gameObject.SetActive(true);
+				elapsed = 0.0f;
+				currentChild++;
+			}
+		}
+	}
+}
