c6c7b04728c8da8bad6fd29f76f2b24f81192962
Ben Frye <ben.j.frye@gmail.com>
2016-12-11 15:37:23 +1100

    Added Splash Screen

---
+++ Assets/_Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	void LateUpdate()
+	{
+		if (!ScreenFade.isBlack && ScreenFade.complete)
+			ScreenFade.isBlack = true;
+		else if (ScreenFade.isBlack && ScreenFade.complete)
+			SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+	}
+}
