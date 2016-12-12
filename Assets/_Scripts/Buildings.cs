b11e630ef81c6a8a9971de5d1012e0b243c59ba1
Ben Frye <ben.j.frye@gmail.com>
2016-12-12 16:18:06 +1100

    Fixed Bug where you couldn't get past Buildings puzzle

--- Assets/_Scripts/Buildings.cs
+++ Assets/_Scripts/Buildings.cs
@@ -1,31 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Buildings : MonoBehaviour {
+public class Buildings : MonoBehaviour
+{
 
 	public string answer;
 
 	private Button restore;
-	private Textbox textbox;
+	private Textbox equation_1;
 
 	void Awake()
 	{
 		restore = transform.FindChild("restore").GetComponent<Button>();
-		textbox = transform.FindChild("x_1").GetComponent<Textbox>();
+		equation_1 = transform.FindChild("equation_1").GetComponent<Textbox>();
 		restore.gameObject.SetActive(false);
 	}
 
-	void Update () 
+	void Update()
 	{
-		if (textbox.entered == answer)
+		if (equation_1.entered == answer)
 			restore.gameObject.SetActive(true);
 		else
 			restore.gameObject.SetActive(false);
 
 		if (restore.clicked)
 		{
-			WorldController.UnlockElement("Buildings");
 			Desktop.buildings_complete = true;
+			WorldController.UnlockElement("Buildings");
 			PuzzleController.LoadPuzzle("Desktop");
 		}
 	}
